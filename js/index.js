7e5c854da624bae948e6c778095c22fd09f1defd
tasmia-rafiq <tasmiarafiq14033@gmail.com>
2022-08-06 17:29:20 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Scroll navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+let nav_link = nav.getElementsByClassName('nav-link');
+for (var i = 0; i < nav_link.length; i++) {
+  nav_link[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
